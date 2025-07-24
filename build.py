262713d50a0bd9c657ff833558517867434e830f
Milo <perrut@proton.me>
2025-07-24 17:49:27 -0300

    Letra
    Letra

---
+++ build.py
@@ -0,0 +1,48 @@
+# The Housemartins - Build
+
+print('Você gostaria de saber a letra da música "Build" de The Housemartins?')
+resposta = input("Sim ou não? ").strip().lower()
+if resposta == "sim":
+    print("""
+Clambering men in big bad boots
+Dug up my den, dug up my roots.
+Treated us like plasticine town
+They built us up and knocked us down.
+From Meccano to Legoland,
+Here they come with a brick in their hand,
+Men with heads filled up with sand,
+Let's build.
+Let's build a house where we can stay [ba ba ba ba build]
+Add a new bit everyday [ba ba ba ba build]
+Let's build a road for us to cross [ba ba ba ba build]
+Build us lots and lots and lots and lots [ba ba ba ba build]
+Whistling men in yellow vans
+They came and drew us diagrams.
+Showed us how it all worked out
+And wrote it down in case of doubt.
+Slow, slow and quick, quick, quick
+It's wall to wall and brick to brick
+They work so fast it makes you sick
+Let's build.
+Let's build a house where we can stay [ba ba ba ba build]
+Add a new bit everyday [ba ba ba ba build]
+Let's build a road for us to cross [ba ba ba ba build]
+Build us lots and lots and lots and lots [ba ba ba ba build]
+Oh, build.
+It's build________________
+It's build________________
+Down with sticks and up with bricks
+In with boots and up with roots
+It's in with suits and new recruits
+It's build...it's build
+Let's build a house where we can stay [ba ba ba ba build]
+Add a new bit everyday [ba ba ba ba build]
+Let's build a road for us to cross [ba ba ba ba build]
+Build us lots and lots and lots and lots [ba ba ba ba build]
+Let's build a house where we can stay [ba ba ba ba build]
+Add a new bit everyday [ba ba ba ba build]
+Let's build a road for us to cross [ba ba ba ba build]
+Build us lots and lots and lots and lots [ba ba ba ba build]
+""")
+else:
+    print("Ok, talvez outra hora!")
